2c0322e7bbc5f07ed288727f4ea7748f22f840af
cory spitzer & kha le <student@epicodus.com>
2015-04-22 11:25:24 -0700

    Add Story and Form

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@ require("sinatra")
 require("sinatra/reloader")
 
 get("/") do
-  "hello world"
+  @friend = "kha"
+  erb(:letter)
 
 end
+
+get("/australia") do
+  @friend = "eric"
+  erb(:australia)
+end
+
+get('/story') do
+  @person1 = params.fetch('person1')
+  @person2 = params.fetch('person2')
+  @animal = params.fetch('animal')
+  @exclamation = params.fetch('exclamation')
+  @verb = params.fetch('verb')
+  @noun = params.fetch('noun')
+  erb(:story)
+end
+
+get("/form") do
+  erb(:form)
+end  
